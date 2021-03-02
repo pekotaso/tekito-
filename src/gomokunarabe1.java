53a02260d5aed00432c2063a895abcf3259ac860
pekotaso <pekotaso@epg.jp>
2021-03-02 23:47:14 +0900

    五目並べ1問目

---
+++ src/gomokunarabe1.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class gomokunarabe1 {
+    public static void main(String[] args) {
+        // 標準入力
+        Scanner sc = new Scanner(System.in);
+        while (sc.hasNextLine()) {
+            String line = sc.nextLine();
+            System.out.println(line);
+        }
+    }
+}
